83598d8cc3b492effeb9d487f82dcfa987ca7fa1
SausePip <user19913@rambler.ru>
2024-09-16 21:08:33 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ def print_params(a=1, b='string', c=True):
     print(a, b, c)
 
 print_params()
+print_params(2, 'brrr')
+print_params('skr', None, 2.4)
+# print_params(3, 5, 3, 6) # ошибка
+print_params(b = 25)
+print_params(c = [1, 2, 3])
+
 
 values_list = [5, 4, 'three']
 values_dict = {'a':1, 'b':'hello', 'c':False}
